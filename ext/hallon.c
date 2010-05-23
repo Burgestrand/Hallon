6ac6db8377218a5ef0456659a04340900079b9ec
Kim Burgestrand <kim@burgestrand.se>
2010-05-23 15:06:14 +0200

    Add Hallon::Error#message

--- ext/hallon.c
+++ ext/hallon.c
@@ -9,10 +9,30 @@
 // API Hierarchy
 static VALUE mHallon;
 
+  static VALUE eError;
+
+
+// Error exception
+// ---------------------------------------------------------------------
+
+/**
+ * call-seq:
+ *   Error.message(fixnum) -> string
+ */
+static VALUE eError_message(VALUE klass, VALUE code)
+{
+  Check_Type(code, T_FIXNUM);
+  return rb_str_new2(sp_error_message(FIX2INT(code)));
+}
+
 // Extension initialization
 void Init_hallon()
 {
   // Top-level module
   mHallon = rb_define_module("Hallon");
   rb_define_const(mHallon, "API_VERSION", INT2FIX(SPOTIFY_API_VERSION));
+  
+  // Error exception
+  eError = rb_define_class_under(mHallon, "Error", rb_eStandardError);
+  rb_define_singleton_method(eError, "message", eError_message, 1);
 }
--- spec/hallon_spec.rb
+++ spec/hallon_spec.rb
@@ -4,4 +4,10 @@ describe Hallon do
   it "should be up to date" do
     Hallon::API_VERSION.should == 4
   end
+
+  describe Hallon::Error do
+    it "should translate error codes" do
+      Hallon::Error.message(1).should == 'Invalid library version'
+    end
+  end
 end
