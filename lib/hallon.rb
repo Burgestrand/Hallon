0d715ee0ee2f39d5e26022a267394b8a2d378997
Kim Burgestrand <kim@burgestrand.se>
2010-05-23 16:15:46 +0200

    Enforce Hallon::Session as a singleton

--- lib/hallon.rb
+++ lib/hallon.rb
@@ -2,3 +2,18 @@
 $:.unshift File.expand_path('./../ext/', File.dirname(__FILE__))
 
 require 'hallon'
+
+module Hallon
+  class Session
+    require 'singleton'
+    include Singleton # Spotify APIv4
+    
+    def self.instance(*args)
+      if @__instance__ and args.length > 0
+        raise ArgumentError, "session has already been initialized"
+      end
+      
+      @__instance__ ||= new *args
+    end
+  end
+end
--- spec/hallon_spec.rb
+++ spec/hallon_spec.rb
@@ -15,17 +15,23 @@ describe Hallon do
     before :all do
       @appkey = IO.read 'spotify_appkey.key'
     end
+    
+    it "should be a singleton" do
+      Hallon::Session.should_not respond_to(:new)
+    end
 
     it "should require an application key" do
-      lambda { Hallon::Session.new }.should raise_error(ArgumentError)
+      lambda { Hallon::Session.instance }.should raise_error(ArgumentError)
     end
 
-    it "should fail with an invalid application key" do
-      lambda { Hallon::Session.new 'invalid' }.should raise_error(Hallon::Error)
+    it "should only accept a valid application key" do
+      lambda { Hallon::Session.instance 'invalid' }.should raise_error(Hallon::Error)
+      lambda { Hallon::Session.instance @appkey }.should_not raise_error
     end
-
-    it "should succeed with a valid application key" do
-      lambda { Hallon::Session.new @appkey }.should_not raise_error
+    
+    it "should not accept arguments after instantiation" do
+      lambda { Hallon::Session.instance @appkey }.should raise_error(ArgumentError)
+      lambda { Hallon::Session.instance }.should_not raise_error
     end
   end
 end
