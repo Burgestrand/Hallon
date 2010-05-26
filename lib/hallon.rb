fe8274dd6f6297cd8fee8e46f84c59e686473522
Kim Burgestrand <kim@burgestrand.se>
2010-05-26 18:54:49 +0200

    Update rdoc comments in hallon.rb

--- lib/hallon.rb
+++ lib/hallon.rb
@@ -5,19 +5,18 @@ require 'singleton'
 # libspotify[https://developer.spotify.com/en/libspotify/overview/] bindings for Ruby!
 module Hallon
   # Thrown by Hallon::Session on Spotify errors.
-  class Error
+  class Error < StandardError
   end
   
-  # Main workhorse of Hallon. You must first initialize an instance to fly
-  # to never-never land!
+  # Main workhorse of Hallon!
   class Session
     include Singleton # Spotify APIv4
     
-    def self.instance(*args) # :nodoc:
+    def self.instance(*args)
       if @__instance__ and args.length > 0
         raise ArgumentError, "session has already been initialized"
       end
-      
+
       @__instance__ ||= new *args
     end
   end
