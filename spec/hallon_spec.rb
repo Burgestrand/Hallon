08e8c69b7ec8f6b7a6143b6afb9611cf7586810b
Kim Burgestrand <kim@burgestrand.se>
2010-06-04 21:46:27 +0200

    Add test for upcoming playlist creation

--- spec/hallon_spec.rb
+++ spec/hallon_spec.rb
@@ -47,4 +47,28 @@ describe Hallon do
       @session.logged_in?.should equal(false)
     end
   end
+  
+  describe Hallon::Playlist do
+    before :all do
+      @session = Hallon::Session.instance.login(USERNAME, PASSWORD)
+      @session.logged_in?.should equal(true)
+      @container = @session.playlists
+    end
+    
+    it "should validate playlist name before creation" do
+      # Not only spaces
+      lambda { @container.add " " }.should raise_error(ArgumentError)
+      
+      # 0 < x < 256
+      lambda { @container.add "" }.should raise_error(ArgumentError)
+      lambda { @container.add("a" * 256) }.should raise_error(ArgumentError)
+    end
+    
+    it "should be possible to create new playlists" do
+      length = @container.length
+      @playlist = @container.add "rspec"
+      @container.length.should equal(length + 1)
+      @playlist.name.should == "rspec"
+    end
+  end
 end
