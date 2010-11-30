681ac612003c9d98c8f48be7895c00e2f7d003d2
Kim Burgestrand <kim@burgestrand.se>
2010-11-30 01:54:03 +0100

    Rename hallon.h to common.h and add real hallon.h

--- ext/hallon/hallon.c
+++ ext/hallon/hallon.c
@@ -1,10 +1,11 @@
-#include "hallon.h"
+#include "common.h"
 
 void Init_hallon(void)
 {
-  VALUE mHallon;
-  
   mHallon = rb_define_module("Hallon");
-    /* libspotify version Hallon was compiled with */
-    rb_define_const(mHallon, "API_VERSION", INT2FIX(SPOTIFY_API_VERSION));
+  
+  /*
+    libspotify API version Hallon was compiled with
+  */
+  rb_define_const(mHallon, "API_VERSION", INT2FIX(SPOTIFY_API_VERSION));
 }
--- ext/hallon/hallon.h
+++ ext/hallon/hallon.h
@@ -1,12 +1,4 @@
-#ifndef __HALLON__
-  #define __HALLON__
-  
-  #include <ruby.h>
-  #include <pthread.h>
-  
-  #ifdef HAVE_LIBSPOTIFY_API_H
-  #  include <libspotify/api.h>
-  #else
-  #  include <spotify/api.h>
-  #endif
-#endif
+/*
+  Hallon main module.
+*/
+static VALUE mHallon;
