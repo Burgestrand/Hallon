c636f60250b9b6d881b9e7b1f8ca9f6d2b81a04d
Kim Burgestrand <kim@burgestrand.se>
2010-12-07 12:42:56 +0100

    Add Session#logged_in?/disconnected?/logged_out?

--- lib/hallon/session.rb
+++ lib/hallon/session.rb
@@ -26,5 +26,23 @@ module Hallon
     def Session.instance(*args, &block)
       @__instance__ ||= new(*args, &block)
     end
+    
+    # True if currently logged in.
+    # @see #state
+    def logged_in?
+      state == :logged_in
+    end
+    
+    # True if logged out.
+    # @see #state
+    def logged_out?
+      state == :logged_out
+    end
+    
+    # True if session has been disconnected.
+    # @see #state
+    def disconnected?
+      state == :disconnected
+    end
   end
 end
