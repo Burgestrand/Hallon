d31a70c779c6b2147ededb1aff7e143caab8d686
Kim Burgestrand <kim@burgestrand.se>
2010-12-27 00:13:28 +0100

    Enforce ruby v1.9 in extconf.rb

--- ext/hallon/extconf.rb
+++ ext/hallon/extconf.rb
@@ -1,25 +1,30 @@
 require 'mkmf'
 
+def error(message)
+  abort "[ERROR] #{message}"
+end
+
 # For Mac OS if installed in /Library/Frameworks/libspotify.framework/
 with_ldflags('-framework libspotify') { RUBY_PLATFORM.match 'darwin' }
 with_cflags('-pipe -ggdb -O0 -Wall') { ENV['DEBUG'] }
 
 # check for ruby!
-abort 'error: Missing ruby header' unless have_header 'ruby.h'
+error 'Missing ruby header' unless have_header 'ruby.h'
+error 'Hallon requires ruby ~> 1.9' unless RUBY_VERSION =~ /\A1\.9/
 
 # check for libspotify
 unless have_func 'sp_session_release' or have_library 'spotify', 'sp_session_release'
-  abort 'error: libspotify not installed'
+  error 'libspotify not installed'
 end
 
 # check for spotify API header
 unless have_header 'libspotify/api.h' or have_header 'spotify/api.h'
-  abort 'error: (lib)spotify/api.h missing'
+  error '(lib)spotify/api.h missing'
 end
 
 # make sure we have pthread support
 unless have_library 'pthread', 'pthread_mutex_lock'
-  abort 'error: missing posix thread-support'
+  error 'missing posix thread-support'
 end
 
 create_makefile 'hallon'
