e42ffafa1b0bf72b0c7d4349400aad6244e1d3c4
Kim Burgestrand <kim@burgestrand.se>
2010-12-28 01:23:33 +0100

    … remove the bad coding style in callbacks.c

--- ext/hallon/callbacks.c
+++ ext/hallon/callbacks.c
@@ -5,23 +5,42 @@
 
 #define SESSION_EVENT_CREATE(_session_ptr, _handler, _data) do {\
   hn_session_data_t *_session_data = (hn_session_data_t*) sp_session_userdata(_session_ptr);\
+  DEBUG("!! " #_handler " !!");\
   EVENT_CREATE(_session_data->event, _handler, _data, &_session_data->event_cond, &_session_data->event_mutex);\
+  DEBUG("** " #_handler " **");\
 } while(0)
 
 /*
   Session callbacks
-  -----------------
+  
+  @see http://developer.spotify.com/en/libspotify/docs/structsp__session__callbacks.html
 */
 
-static VALUE ruby_notify_main_thread(void *args)
-{ return rb_ary_new3(1, STR2SYM("process_events")); }
-static void callback_process_events(sp_session *session_ptr)
-{ DEBUG("!! notify"); SESSION_EVENT_CREATE(session_ptr, ruby_notify_main_thread, NULL); DEBUG("*notify*"); }
+/*
+  Called when processing needs to take place on the main thread.
+*/
+static VALUE ruby_process_events(void *args)
+{
+  return rb_ary_new3(1, STR2SYM("process_events"));
+}
 
-static VALUE ruby_logged_in(void *args)
-{ return rb_ary_new3(2, STR2SYM("logged_in"), INT2FIX((sp_error)args)); }
+static void callback_process_events(sp_session *session_ptr)
+{
+  SESSION_EVENT_CREATE(session_ptr, ruby_process_events, NULL);
+}
+
+/*
+  Called when login has been processed and was successful.
+*/
+static VALUE ruby_logged_in(void *error)
+{
+  return rb_ary_new3(2, STR2SYM("logged_in"), INT2FIX((sp_error)error));
+}
+
 static void callback_logged_in(sp_session *session_ptr, sp_error error)
-{ DEBUG("!! logged_in"); SESSION_EVENT_CREATE(session_ptr, ruby_logged_in, (void*) error); DEBUG("*logged_in*"); }
+{
+  SESSION_EVENT_CREATE(session_ptr, ruby_logged_in, (void*) error);
+}
 
 // @see session.c
 const sp_session_callbacks HALLON_SESSION_CALLBACKS = 
