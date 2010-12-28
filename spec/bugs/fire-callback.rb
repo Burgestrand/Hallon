10a01e0321b5d9f91828f352b16f3802a86cd4da
Kim Burgestrand <kim@burgestrand.se>
2010-12-28 03:13:43 +0100

    … correct the example of bug-demonstration

--- spec/bugs/fire-callback.rb
+++ spec/bugs/fire-callback.rb
@@ -2,12 +2,16 @@ $: << File.expand_path('../../../lib', __FILE__)
 require 'hallon'
 require_relative '../support/config'
 
-class Session
-  private
-    def noop(*args)
-      # noop
-    end
+module Hallon
+  class Session
+    private
+      def noop(*args)
+        # noop
+      end
+  end
 end
 
 sess = Hallon::Session.instance Hallon::APPKEY
 5.times { sess.fire! :noop }
+
+sleep 2
