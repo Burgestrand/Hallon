a255f098e677cc053ac8a8eafc0d4854be08d887
Kim Burgestrand <kim@burgestrand.se>
2010-12-29 04:04:49 +0100

    Update fire-callback.rb to use new event system

--- spec/bugs/fire-callback.rb
+++ spec/bugs/fire-callback.rb
@@ -2,16 +2,24 @@ $: << File.expand_path('../../../lib', __FILE__)
 require 'hallon'
 require_relative '../support/config'
 
+Thread.abort_on_exception = true
+
 module Hallon
   class Session
-    private
-      def noop(*args)
-        # noop
+    class Callbacks
+      def destroy
+        throw :shuriken
       end
+    
+      def noop
+        puts "Noop"
+      end
+    end
   end
 end
 
-sess = Hallon::Session.instance Hallon::APPKEY
+sess = Hallon::Session.instance(Hallon::APPKEY)
 
 5.times { sess.fire! :noop }
-sleep 5
+sess.fire! :destroy
+sleep 2
