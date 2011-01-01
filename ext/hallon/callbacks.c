4133eb55f12131d5f952374a12a49eab2d8d30b9
Kim Burgestrand <kim@burgestrand.se>
2011-01-01 23:24:19 +0100

    Define C and Ruby handlers for a bunch more session callbacks

--- ext/hallon/callbacks.c
+++ ext/hallon/callbacks.c
@@ -10,6 +10,18 @@
                session_data->event, _handler, _data);\
 } while(0)
 
+/*
+  Most of these callbacks are just boilerplate. These macros makes it a little
+  wee bit easier.
+*/
+#define DEFINE_NAMED_CALLBACK(name) \
+  static VALUE ruby_##name(void *data) { return rb_ary_new3(1, STR2SYM(#name)); }\
+  static void c_##name(sp_session *session_ptr) { SESSION_EVENT_CREATE(session_ptr, ruby_##name, NULL); }
+
+#define DEFINE_ERROR_CALLBACK(name) \
+  static VALUE ruby_##name(void *error) { return rb_ary_new3(2, STR2SYM(#name), INT2FIX((sp_error)error)); }\
+  static void c_##name(sp_session *session_ptr, sp_error error) { SESSION_EVENT_CREATE(session_ptr, ruby_##name, (void*)error); }
+
 
 /*
   Used to be for debugging purposes only, but now it is used to fire arbitrary
@@ -33,48 +45,40 @@ VALUE hn_session_fire(void *_argv)
   @see http://developer.spotify.com/en/libspotify/docs/structsp__session__callbacks.html
 */
 
-/*
-  Called when processing needs to take place on the main thread.
-*/
-static VALUE ruby_process_events(void *args)
-{
-  return rb_ary_new3(1, STR2SYM("process_events"));
-}
+/* The simple callbacks, with nothing but names and no arguments. */
+DEFINE_NAMED_CALLBACK(process_events);
+DEFINE_NAMED_CALLBACK(logged_out);
+DEFINE_NAMED_CALLBACK(metadata_updated);
+DEFINE_NAMED_CALLBACK(play_token_lost);
+DEFINE_NAMED_CALLBACK(userinfo_updated);
+DEFINE_NAMED_CALLBACK(end_of_track);
+DEFINE_NAMED_CALLBACK(start_playback);
+DEFINE_NAMED_CALLBACK(stop_playback);
 
-static void callback_process_events(sp_session *session_ptr)
-{
-  SESSION_EVENT_CREATE(session_ptr, ruby_process_events, NULL);
-}
+/* Trickier callbacks: error argument! */
+DEFINE_ERROR_CALLBACK(logged_in);
+DEFINE_ERROR_CALLBACK(connection_error);
+DEFINE_ERROR_CALLBACK(streaming_error);
 
-/*
-  Called when login has been processed and was successful.
-*/
-static VALUE ruby_logged_in(void *error)
-{
-  return rb_ary_new3(2, STR2SYM("logged_in"), INT2FIX((sp_error)error));
-}
+/* Even trickier: string argument! (does stack get popped? do we need to copy the string?) */
 
-static void callback_logged_in(sp_session *session_ptr, sp_error error)
-{
-  SESSION_EVENT_CREATE(session_ptr, ruby_logged_in, (void*) error);
-}
 
 /* @see session.c */
 const sp_session_callbacks HALLON_SESSION_CALLBACKS = 
 {
- .logged_in              = callback_logged_in,
- .logged_out             = NULL,
- .metadata_updated       = NULL,
- .connection_error       = NULL,
+ .logged_in              = c_logged_in,
+ .logged_out             = c_logged_out,
+ .metadata_updated       = c_metadata_updated,
+ .connection_error       = c_connection_error,
  .message_to_user        = NULL,
- .play_token_lost        = NULL,
+ .play_token_lost        = c_play_token_lost,
  .streaming_error        = NULL,
  .log_message            = NULL,
- .userinfo_updated       = NULL,
- .notify_main_thread     = callback_process_events,
+ .userinfo_updated       = c_userinfo_updated,
+ .notify_main_thread     = c_process_events,
  .music_delivery         = NULL,
- .end_of_track           = NULL,
- .start_playback         = NULL,
- .stop_playback          = NULL,
+ .end_of_track           = c_end_of_track,
+ .start_playback         = c_start_playback,
+ .stop_playback          = c_stop_playback,
  .get_audio_buffer_stats = NULL
 };
