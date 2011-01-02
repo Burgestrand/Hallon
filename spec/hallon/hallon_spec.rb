4b8322c9fc6650a98861f8f091467feeba67a0fe
Kim Burgestrand <kim@burgestrand.se>
2011-01-03 00:38:57 +0100

    Extract #example_uris to a support/-file

--- spec/hallon/hallon_spec.rb
+++ spec/hallon/hallon_spec.rb
@@ -10,12 +10,7 @@ describe Hallon do
   describe "URI" do
     subject { Hallon::URI }
     
-    %w(spotify:search:omg%2bwtf%2b%ef%a3%bf%c3%9f%e2%88%82%2bbbq
-       spotify:track:3oN2Kq1h07LSSBSLYQp0Ns
-       spotify:album:6I58XCEkOnfUVsfpDehzlQ
-       spotify:artist:6MF9fzBmfXghAz953czmBC
-       spotify:user:burgestrand:playlist:4nQnbGi4kALbME9csEqdW2
-       spotify:user:burgestrand).each do |uri|
+    example_uris.keys.each do |uri|
       specify { should match uri }
     end
   end
---
+++ spec/support/example_uris.rb
@@ -0,0 +1,11 @@
+def example_uris
+  {
+    "spotify:search:omg%2bwtf%2b%ef%a3%bf%c3%9f%e2%88%82%2bbbq" => :search,
+    "spotify:track:3oN2Kq1h07LSSBSLYQp0Ns" => :track,
+    "spotify:album:6I58XCEkOnfUVsfpDehzlQ" => :album,
+    "spotify:artist:6MF9fzBmfXghAz953czmBC" => :artist,
+    "spotify:user:burgestrand:playlist:4nQnbGi4kALbME9csEqdW2" => :playlist,
+    "spotify:user:burgestrand" => :profile,
+    "spotify:user:burgestrand:starred" => :starred,
+  }
+end
