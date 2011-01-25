eb4a0a8f1ec76c3a89b0de04faf743591f652f8c
Kim Burgestrand <kim@burgestrand.se>
2011-01-25 04:33:37 +0100

    Fix issue in Mac OS when libspotify installed as non-framework

--- ext/hallon/extconf.rb
+++ ext/hallon/extconf.rb
@@ -4,22 +4,16 @@ def error(message)
   abort "[ERROR] #{message}"
 end
 
-# For Mac OS if installed in /Library/Frameworks/libspotify.framework/
-with_ldflags('-framework libspotify') { RUBY_PLATFORM.match 'darwin' }
-
-# check for ruby!
 error 'Missing ruby header' unless have_header 'ruby.h'
 error 'Hallon requires ruby ~> 1.9' unless RUBY_VERSION =~ /\A1\.9/
 
-# check for libspotify
-unless have_func 'sp_session_release' or have_library 'spotify', 'sp_session_release'
-  error 'libspotify not installed'
-end
-
-# check for spotify API header
-unless have_header 'libspotify/api.h' or have_header 'spotify/api.h'
-  error '(lib)spotify/api.h missing'
-end
+checking_for 'libspotify' do
+  with_ldflags('-framework libspotify') do
+    try_link 'int main(void) { return 0; }'
+  end unless have_library 'spotify'
+  
+  have_func 'sp_session_create', 'libspotify/api.h'
+end or error 'libspotify not installed'
 
 # make sure we have pthread support
 unless have_library 'pthread', 'pthread_mutex_lock'
