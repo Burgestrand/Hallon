c51be8baf6e25918fb8012449c957b55e9537937
Kim Burgestrand <kim@burgestrand.se>
2011-03-17 14:49:18 +0100

    Implement Hallon::Session#initialize (without callbacks)

--- lib/hallon/session.rb
+++ lib/hallon/session.rb
@@ -26,6 +26,50 @@ module Hallon
       @__instance__ ||= new(*args, &block)
     end
     
+    # Create a new Spotify session.
+    # 
+    # @param [#to_s] appkey
+    # @param [Hash] options
+    # @option options [String] :user_agent ("Hallon") User-Agent to use (length < 256)
+    # @option options [String] :settings_path ("tmp") where to save settings and user-specific cache
+    # @option options [String] :cache_path ("") where to save cache files (set to "" to disable)
+    # @option options [Bool]   :load_playlists (true) load playlists into RAM on startup
+    # @option options [Bool]   :compress_playlists (true) compress local copies of playlists
+    # @option options [Bool]   :cache_playlist_metadata (true) cache metadata for playlists locally
+    # @yield allows you to define handlers for events (see {#on})
+    # @raise [ArgumentError] if options[:user_agent] is more than 256 characters long
+    # @raise [Hallon::Error] if `sp_session_create` fails
+    # @see http://developer.spotify.com/en/libspotify/docs/structsp__session__config.html
+    def initialize(appkey, options = {}, &block)
+      @appkey  = appkey.to_s
+      @options = {
+        :user_agent => "Hallon",
+        :settings_path => "tmp",
+        :cache_path => "",
+        :load_playlists => true,
+        :compress_playlists => true,
+        :cache_playlist_metadata => true
+      }.merge(options)
+      
+      if @options[:user_agent].length > 256
+        raise ArgumentError, "User-agent must be less than 256 characters long"
+      end
+      
+      config  = Spotify::SessionConfig.new
+      config[:api_version]     = Spotify::API_VERSION
+      config.application_key   = @appkey
+      options.each_pair do |(key, value)|
+        config.send(:"#{key}=", value)
+      end
+      config.callbacks         = nil
+      
+      # You pass a pointer to the session pointer to libspotify >:)
+      FFI::MemoryPointer.new(:pointer) do |p|
+        Hallon::Error::maybe_raise Spotify::session_create(config, p)
+        @session = Spotify::Pointer.new(p.read_pointer, :session)
+      end
+    end
+    
     # True if currently logged in.
     # @see #status
     def logged_in?
