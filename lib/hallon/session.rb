40ee526585979f7c23af14aca0a6423b04fcbf2c
Kim Burgestrand <kim@burgestrand.se>
2011-03-17 14:41:53 +0100

    Remove Session#merge_defaults and callback threads

--- lib/hallon/session.rb
+++ lib/hallon/session.rb
@@ -3,10 +3,8 @@ require 'singleton'
 
 module Hallon
   class Session
-    
     # The options Hallon used at {Session#initialize}.
     # 
-    # @see Session#merge_defaults
     # @return [Hash]
     attr_reader :options
     
@@ -45,72 +43,5 @@ module Hallon
     def disconnected?
       status == :disconnected
     end
-    
-    private
-      # Merge the given hash with default options for {#initialize}.
-      #
-      # @note This is called automatically by {#initialize}.
-      # @return [Hash]
-      def merge_defaults(options)
-        options = options || {}        
-        {
-          :user_agent => "Hallon",
-          :settings_path => "tmp",
-          :cache_path => "",
-          
-          :load_playlists => true,
-          :compress_playlists => true,
-          :cache_playlist_metadata => true
-        }.merge(options)
-      end
-      
-      # Spawns the Event Dispatcher.
-      # 
-      # This is one of two threads responsible for handling events fired
-      # by `libspotify`. The other one is in `ext/hallon/callbacks.c`,
-      # responsible for sending events to this thread.
-      # 
-      # Events are received by reading from the given queue; each item
-      # in the queue is expected to be an array containing *at least*
-      # two items: an object to handle the event, and the event name.
-      # Any following items will be considered arguments to the handler.
-      # 
-      # The event name is prefixed with “on_” and then dispatched to
-      # the handler: `handler.on_#{method}(*args)`
-      # 
-      # @param [Queue] queue used to read incoming events from
-      # @return [Thread]
-      def spawn_dispatcher(queue)
-        Thread.new do
-          loop do
-            handler, method, *args = queue.pop
-            method = :"on_#{method}"
-
-            begin
-              handler.public_send(method, *args)
-            rescue => e
-              warn "#{handler}##{method}(#{args.join(', ')}) => #{e.inspect}"
-            end if handler.respond_to?(method)
-          end
-        end
-      end
-      
-      # Spawns both the Taskmaster (callbacks.c) and the Dispatcher.
-      # 
-      # libspotify calls out to C functions when events occur, but
-      # unfortunately this is done in an internal libspotify thread.
-      # This means we are unable to call ruby functions directly in the
-      # callbacks, and must find a way to bring the events and their
-      # data into Ruby; the taskmaster and the dispatcher is how.
-      # 
-      # - libspotify is an event producer for the taskmaster
-      # - the taskmaster is an event producer for the dispatcher
-      # - the dispatcher executes the correct handler for each event
-      # 
-      # @return [Array<Thread, Thread>] (taskmaster, dispatcher)
-      def spawn_handlers
-        queue = Queue.new
-        [spawn_taskmaster(queue), spawn_dispatcher(queue)]
-      end
   end
 end
--- spec/hallon/session_spec.rb
+++ spec/hallon/session_spec.rb
@@ -1,10 +1,6 @@
-require 'thread'
-require 'stringio'
-
 describe Hallon::Session do
   # Hallon::Session#instance requires that a session have NOT been established,
   # thus its’ tests are declared in the spec_helper.rb
-  
   context "once instantiated" do
     it_behaves_like "spotify objects" do
       describe "appkey" do
@@ -22,20 +18,6 @@ describe Hallon::Session do
         its([:cache_playlist_metadata]) { should == true }
       end
     
-      describe "#merge_defaults" do
-        define_method(:merge_defaults) do |*args|
-          session.send(:merge_defaults, *args)
-        end
-        
-        it "should return the defaults if no options given" do
-          merge_defaults(nil).should be_a Hash # values not important
-        end
-      
-        it "should allow given options to override defaults" do
-          merge_defaults(:user_agent => "Cow")[:user_agent].should == "Cow"
-        end
-      end
-    
       describe "#process_events" do
         it "should return the timeout" do
           session.process_events.should be_a Fixnum
@@ -50,45 +32,6 @@ describe Hallon::Session do
         it "should check logged in status" do
           session.should_receive(:logged_in?).once.and_return(false)
           expect { session.logout }.to_not raise_error
-        end
-      end
-      
-      describe "dispatcher thread" do
-        def dispatch(*arg, &block)
-          raise ArgumentError, "no block given" unless block_given?
-          m = Mutex.new
-          c = ConditionVariable.new
-          o = Object.new
-          result = nil
-          
-          o.define_singleton_method(:on_get_thread) do |*args|
-            begin
-              result = block.call(*args)
-            ensure
-              m.synchronize { c.signal }
-            end
-          end
-          
-          m.synchronize do
-            session.fire!(o, :get_thread, *arg)
-            c.wait(m)
-          end
-          
-          result
-        end
-
-        it "should not equal current thread" do
-          dispatch { Thread.current }.should_not equal Thread.current
-        end
-        
-        it "should print a warning when an error is raised" do
-          stderr = $stderr
-          $stderr = io = StringIO.new
-          dispatch { raise "omgwtf" }
-          $stderr = stderr
-          io.rewind
-          
-          io.read.should match 'RuntimeError: omgwtf'
         end
       end
     end
