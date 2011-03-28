bf54be9df2edb34264133488b6e1f8d55db698ea
Kim Burgestrand <kim@burgestrand.se>
2011-03-29 01:00:14 +0200

    Add specification for Hallon::Base#on when calling without instance_eval

--- spec/hallon/base_spec.rb
+++ spec/hallon/base_spec.rb
@@ -15,5 +15,10 @@ describe Hallon::Base do
 
   describe "#on" do
     it { should respond_to :on_fired_event }
+    
+    it "should define methods without instance_eval" do
+      subject.on(:method_call) {}
+      should respond_to :on_method_call
+    end
   end
 end
