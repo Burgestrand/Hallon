1454ba9620926cd418461d5611bca12c1fa77265
Kim Burgestrand <kim@burgestrand.se>
2011-04-21 00:33:06 +0200

    Add Base#protecting_handlers(&block)

--- lib/hallon/base.rb
+++ lib/hallon/base.rb
@@ -17,5 +17,27 @@ module Hallon
     def on(event, &block)
       define_singleton_method(:"on_#{event}", &block)
     end
+    
+    # Run the given block, protecting all previous event handlers.
+    # 
+    # @yield
+    # @return whatever the given block returns
+    def protecting_handlers
+      on_method = method(:on)
+      overridden_callbacks = {}
+      
+      define_singleton_method(on_method.name) do |event, &block|
+        handler_name = :"on_#{event}"
+        overridden_callbacks[handler_name] = method(handler_name)
+        on_method.call(event, &block)
+      end
+      
+      yield
+    ensure
+      define_singleton_method(on_method.name, &on_method)
+      overridden_callbacks.each_pair do |event, handler|
+        define_singleton_method(event, handler)
+      end
+    end
   end
 end
--- spec/hallon/base_spec.rb
+++ spec/hallon/base_spec.rb
@@ -19,4 +19,32 @@ describe Hallon::Base do
       should respond_to :on_method_call
     end
   end
+  
+  describe "#protecting_handlers" do
+    it "should call the given block and return its value" do
+      subject.protecting_handlers do
+        "was called"
+      end.should eq "was called"
+    end
+    
+    it "should restore the previous handler on exit" do
+      subject.on(:before_block) { "unchanged" }
+      
+      subject.protecting_handlers do
+        subject.on(:before_block) { "changed" }
+        subject.on_before_block.should eq "changed"
+      end
+      
+      subject.on_before_block.should eq "unchanged"
+    end
+    
+    specify "#on should still work properly afterwards" do
+      subject.protecting_handlers do
+        # nothing!
+      end
+      
+      subject.on(:after_block) { |*args| args }
+      subject.on_after_block(1, 2).should eq [1, 2]
+    end
+  end
 end
