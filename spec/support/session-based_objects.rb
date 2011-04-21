af249f6cfa347d40a55d76463fdbc19fd05ae4ea
Kim Burgestrand <kim@burgestrand.se>
2011-04-21 05:13:51 +0200

    Add session as @session object in pre-initialized Session shared spec example

--- spec/support/session-based_objects.rb
+++ spec/support/session-based_objects.rb
@@ -1,5 +1,5 @@
 shared_examples_for "pre-initialized Session" do
   let(:options) { {:user_agent => "Hallon (rspec)", :settings_path => "tmp", :cache_path => "tmp/cache"} }
   let(:session) { Hallon::Session.instance(Hallon::APPKEY, options) }
-  before(:all) { session }
+  before(:all) { @session = session }
 end
