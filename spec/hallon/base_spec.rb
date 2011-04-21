9d9de2871d4ab0d3f2c77633b81f2cf9d546f4b3
Kim Burgestrand <kim@burgestrand.se>
2011-04-21 05:10:27 +0200

    Refactor Base specs to use a new Base instance on each test

--- spec/hallon/base_spec.rb
+++ spec/hallon/base_spec.rb
@@ -1,50 +1,46 @@
 describe Hallon::Base do
-  subject do
-    klass = Class.new do
+  subject do 
+    Class.new do
       include Hallon::Base
     end
-    
-    obj = klass.new
-    obj.on(:fired_event) { :fired! }
-    obj
   end
   
-  its(:public_methods) { should include :on }
+  before(:each) { @subject = subject.new }
+  
+  its(:instance_methods) { should include :on }
 
   describe "#on" do
-    it { should respond_to :on_fired_event }
-    
-    it "should define methods without instance_eval" do
-      subject.on(:method_call) {}
-      should respond_to :on_method_call
+    it "should define the given method with an on_ prefix" do
+      @subject.on(:fired_event) {}
+      @subject.should respond_to :on_fired_event
     end
   end
   
   describe "#protecting_handlers" do
     it "should call the given block and return its value" do
-      subject.protecting_handlers do
+      @subject.protecting_handlers do
         "was called"
       end.should eq "was called"
     end
     
     it "should restore the previous handler on exit" do
-      subject.on(:before_block) { "unchanged" }
+      @subject.on(:before_block) { "unchanged" }
       
-      subject.protecting_handlers do
-        subject.on(:before_block) { "changed" }
-        subject.on_before_block.should eq "changed"
+      @subject.protecting_handlers do
+        @subject.on(:before_block) { "changed" }
+        @subject.on_before_block.should eq "changed"
       end
       
-      subject.on_before_block.should eq "unchanged"
+      @subject.on_before_block.should eq "unchanged"
     end
     
     specify "#on should still work properly afterwards" do
-      subject.protecting_handlers do
+      @subject.protecting_handlers do
         # nothing!
       end
       
-      subject.on(:after_block) { |*args| args }
-      subject.on_after_block(1, 2).should eq [1, 2]
+      @subject.on(:after_block) { |*args| args }
+      @subject.on_after_block(1, 2).should eq [1, 2]
     end
   end
 end
