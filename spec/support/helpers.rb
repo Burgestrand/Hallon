9000669bb7d7172e207016a04f854023a2a31977
Kim Burgestrand <kim@burgestrand.se>
2011-06-04 15:12:31 +0200

    Move all spec helpers into spec/support/helpers

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -8,6 +8,26 @@ RSpec.configure do
   end
 
   def session
-    Hallon::Session.instance(Hallon::APPKEY, options)
+    @session ||= Hallon::Session.instance(Hallon::APPKEY, options)
+  end
+end
+
+shared_examples_for "existing session" do
+  before(:all) { @session = session }
+end
+
+shared_examples_for "logged in" do
+  before(:all) do
+    session.login ENV['HALLON_USERNAME'], ENV['HALLON_PASSWORD']
+
+    session.process_events_on(:logged_in) do
+      session.logged_in?
+    end
+
+    session.should be_logged_in
+  end
+
+  before(:each) do
+    session.should be_logged_in
   end
 end
