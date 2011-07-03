d71767a939a8f83ab59c98c3f023e1749a5d17a8
Kim Burgestrand <kim@burgestrand.se>
2011-07-03 21:09:06 +0200

    Add documentation for FFI::Pointer#read_size_t

--- lib/hallon/ext/ffi.rb
+++ lib/hallon/ext/ffi.rb
@@ -1,5 +1,12 @@
 module FFI
   class Pointer
+    # Document-method: read_size_t
+    #
+    # Read N `size_t` from the start of the pointer.
+    #
+    # @param [Integer] count how many to read
+    # @return a type of appropriate size
+
     type = FFI.find_type(:size_t)
     type, _ = FFI::TypeDefs.find do |(name, t)|
       method_defined? "read_#{name}" if t == type
