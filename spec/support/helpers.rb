25681634d1997c7a4a2a4c47ee132ae05270f18b
Kim Burgestrand <kim@burgestrand.se>
2011-07-06 21:00:53 +0200

    Fix tests hanging when user don’t have premium

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -23,7 +23,11 @@ shared_examples_for "logged in" do
   before(:all) do
     unless session.logged_in?
       session.login ENV['HALLON_USERNAME'], ENV['HALLON_PASSWORD']
-      session.process_events_on(:logged_in) { session.logged_in? }
+      logged_in = session.process_events_on(:logged_in) { |error| error }
+      logged_in.should eq :ok
+
+      finished = session.process_events_on(:connection_error) { |error| session.logged_in? or error }
+      finished.should be_true
     end
 
     session.should be_logged_in
