052836eb827d69235e3d70c909a6650ccb88de3b
Kim Burgestrand <kim@burgestrand.se>
2011-07-09 23:45:03 +0200

    Improve documentation by moving stuff around

--- lib/hallon/ext/ffi.rb
+++ lib/hallon/ext/ffi.rb
@@ -1,17 +1,26 @@
+# Custom extensions to the FFI gem.
+#
+# @see https://github.com/ffi/ffi
 module FFI
+  # FFI::Pointer is the underlying class used to read
+  # and write data to pointers. For more information
+  # see the FFI gem.
   class Pointer
-    # Document-method: read_size_t
-    #
-    # Read N `size_t` from the start of the pointer.
-    #
-    # @param [Integer] count how many to read
-    # @return a type of appropriate size
-
-    type = FFI.find_type(:size_t)
-    type, _ = FFI::TypeDefs.find do |(name, t)|
-      method_defined? "read_#{name}" if t == type
+    type, _ = begin
+      type = FFI.find_type(:size_t)
+      FFI::TypeDefs.find do |(name, t)|
+        method_defined? "read_#{name}" if t == type
+      end
     end
 
-    alias_method :read_size_t, "read_#{type}" if type
+    unless type.nil?
+      # Read N `size_t` from the start of the pointer.
+      #
+      # @param [Integer] count how many to read
+      # @return a type of appropriate size
+      define_method(:read_size_t) do |*args|
+        public_send("read_#{type}", *args)
+      end
+    end
   end
 end
--- lib/hallon/linkable.rb
+++ lib/hallon/linkable.rb
@@ -6,33 +6,39 @@ module Hallon
   # @note Linkable is not part of Hallons’ public API.
   # @private
   module Linkable
+    # These are extended onto a class when {Linkable} is included.
+    module ClassMethods
+      include Forwardable
+
+      # Creates `#convert` method which’ll convert a link to a pointer
+      #
+      # @example
+      #   # Creates instance method `convert(link)`
+      #   link_converter(:playlist) do |link|
+      #     Spotify::link_as_playlist(link)
+      #   end
+      #
+      # @param [Symbol] type expected link type
+      # @yield [link, *args] called when conversion is needed from Link to Pointer
+      # @yieldparam [Hallon::Link] link
+      # @yieldparam *args any extra arguments given to `#convert`
+      # @see Link#pointer
+      def link_converter(type)
+        define_singleton_method(:convert) do |link, *args|
+          if link.is_a? FFI::Pointer then link else
+            yield Link.new(link).pointer(type), *args
+          end
+        end
+
+        def_delegators 'self.class', :convert
+      end
+    end
+
+    # When included, Linkable also defines the {ClassMethods#link_converter} method.
     def self.included(other)
       other.instance_eval do
         private
-          extend Forwardable
-
-          # Creates `#convert` method which’ll convert a link to a pointer
-          #
-          # @example
-          #   # Creates instance method `convert(link)`
-          #   link_converter(:playlist) do |link|
-          #     Spotify::link_as_playlist(link)
-          #   end
-          #
-          # @param [Symbol] type expected link type
-          # @yield [link, *args] called when conversion is needed from Link to Pointer
-          # @yieldparam [Hallon::Link] link
-          # @yieldparam *args any extra arguments given to `#convert`
-          # @see Link#pointer
-          def link_converter(type)
-            define_singleton_method(:convert) do |link, *args|
-              if link.is_a? FFI::Pointer then link else
-                yield Link.new(link).pointer(type), *args
-              end
-            end
-
-            def_delegators 'self.class', :convert
-          end
+          extend ClassMethods
       end
     end
 
