48ccdff94527dea9d4a290fc4525fffc47324ed3
Kim Burgestrand <kim@burgestrand.se>
2011-07-22 04:23:13 +0200

    Shorten logging in example

--- examples/logging_in.rb
+++ examples/logging_in.rb
@@ -9,18 +9,10 @@ session = Hallon::Session.instance IO.read(ENV['HALLON_APPKEY']) do
 end
 
 session.login ENV['HALLON_USERNAME'], ENV['HALLON_PASSWORD']
-logged_in  = session.process_events_on(:logged_in) { |error| error }
 
-unless logged_in == :ok
-  abort "[ERROR] (:logged_in) #{Hallon::Error.explain(logged_in)}"
-end
-
-conn_error = session.process_events_on(:connection_error) do |error|
-  session.logged_in? or error
-end
-
-unless conn_error == true
-  abort "[ERROR] (:connection_error) #{Hallon::Error.explain(conn_error)}"
+session.process_events_on(:logged_in) { |error| Hallon::Error.maybe_raise(error) }
+session.process_events_on(:connection_error) do |error|
+  session.logged_in? or Hallon::Error.maybe_raise(error)
 end
 
 puts "Successfully logged in!"
