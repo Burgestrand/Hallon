305c99753e77b2eb3f4163cbbf963fa4ee28d9bf
Kim Burgestrand <kim@burgestrand.se>
2011-07-24 16:51:36 +0200

    Add documentation to Object extension

--- lib/hallon/ext/object.rb
+++ lib/hallon/ext/object.rb
@@ -1,11 +1,14 @@
+# Extension of Object for Ruby 1.8 compatibility.
 class Object
-  unless defined?(singleton_class)
+  unless method_defined?(:singleton_class)
+    # Singleton class of object.
     def singleton_class
       class << self; self; end
     end
   end
 
   unless method_defined?(:define_singleton_method)
+    # Defines a method on the singleton class of object.
     def define_singleton_method(*args, &b)
       singleton_class.send(:define_method, *args, &b)
     end
