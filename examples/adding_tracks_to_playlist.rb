cdf701bac888bd981ababed7422174b4f5758c65
Kim Burgestrand <kim@burgestrand.se>
2011-11-01 19:02:39 +0100

    Update adding_tracks_to_playlist example

--- examples/adding_tracks_to_playlist.rb
+++ examples/adding_tracks_to_playlist.rb
@@ -14,12 +14,6 @@ track_uris = %w[spotify:track:6GHrP1i3vQo1e8VkEHRnvz]
 # index to add the tracks to in the playlist
 position = 0
 
-##
-# Hallon / Spotify API usage
-#
-# The example below uses a mix of Hallon and Spotify (libspotify-ruby) API;
-# once Hallon covers all of the API usage this document will be updated to
-# reflect the new changes.
 session = Hallon::Session.initialize IO.read(ENV['HALLON_APPKEY']) do
   on(:log_message) do |message|
     puts "[LOG] #{message}"
@@ -35,27 +29,14 @@ puts "Loading playlist #{playlist_uri}"
 playlist = Hallon::Playlist.new(playlist_uri)
 session.wait_for { playlist.loaded? }
 
-# Then we convert all track URIs to actual tracks. Once again, I’m not
-# sure you really need to load all tracks, but better safe than sorry.
+# Convert all track URIs to actual Tracks. Load these too; again, not
+# sure if this is entirely necessary.
 puts "Loading #{track_uris.length} tracks"
 tracks = track_uris.map { |x| Hallon::Track.new(x) }
 session.wait_for { tracks.all?(&:loaded?) }
 
-# Adding tracks to the playlist means we must drop down to raw Spotify API
-# because Hallon does not support it yet. Luckily, it’s still Ruby!
-#
-# Passing a block to FFI::MemoryPointer means it’ll be automatically freed
-# once the block finishes. We must create this pointer because the Spotify
-# API takes a C array of track pointers, so we store them in this pointer.
-FFI::MemoryPointer.new(:pointer, tracks.length) do |tracks_ary|
-  tracks_ary.write_array_of_pointer tracks.map(&:pointer)
-
-  # finally, add all the tracks to the playlist and check for failure
-  print "Adding tracks to playlist: "
-  error = Spotify.playlist_add_tracks(playlist.pointer, tracks_ary, tracks.length, position, session.pointer)
-  puts error
-  Hallon::Error.maybe_raise(error)
-end
+# … insert the tracks at the desired position into the playlist.
+playlist.insert(position, tracks)
 
 # finally, wait for the updates to the playlist to be acknowledged by the
 # Spotify back-end. Once they have, we’ll see the tracks in our desktop
@@ -63,10 +44,12 @@ end
 #
 puts "Uploading playlist changes to Spotify back-end"
 
-# there’s a tiny bug in Playlist#pending? in libspotify that makes it 
-# return false after adding tracks to a playlist unless you first call
-# Session#process_events
-session.process_events
+# there’s a tiny bug (?) in Playlist#pending? in libspotify that makes it
+# return false after adding tracks to a playlist unless you first wait
+# for it to become pending
+session.wait_for { playlist.pending? }
+
+# after that, we can wait for it to become not pending!
 session.wait_for { not playlist.pending? }
 
 puts "We’re done!"
