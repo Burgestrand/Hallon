aa454a759f43eec9e078ac628abf56d8ca217f01
Kim Burgestrand <kim@burgestrand.se>
2011-11-02 00:41:26 +0100

    Update adding_tracks_to_playlist example with latest findings

--- examples/adding_tracks_to_playlist.rb
+++ examples/adding_tracks_to_playlist.rb
@@ -9,7 +9,7 @@ require './spec/support/config'
 playlist_uri = "spotify:user:burgestrand:playlist:07AX9IY9Hqmj1RqltcG0fi"
 
 # array of track uris you wish to add to the playlist
-track_uris = %w[spotify:track:6GHrP1i3vQo1e8VkEHRnvz]
+track_uris = %w[spotify:track:6GHrP1i3vQo1e8VkEHRnvz spotify:track:2t7u74OJXHf0qQxXHpnb2R spotify:track:24q7a0Bo5MFLJUslg1lsS5]
 
 # index to add the tracks to in the playlist
 position = 0
@@ -23,17 +23,16 @@ end
 session.login!(ENV['HALLON_USERNAME'], ENV['HALLON_PASSWORD'])
 puts "Successfully logged in!"
 
-# First, we load the playlist. I’m not entirely sure if this is really
-# necessary, but let’s be on the safe side!
+# First, we load the playlist. This is necessary for libspotify
+# to know if we have write access to it or not. Not doing it might
+# end us up getting permission denied errors later.
 puts "Loading playlist #{playlist_uri}"
 playlist = Hallon::Playlist.new(playlist_uri)
 session.wait_for { playlist.loaded? }
 
-# Convert all track URIs to actual Tracks. Load these too; again, not
-# sure if this is entirely necessary.
+# Convert all track URIs to actual Tracks.
 puts "Loading #{track_uris.length} tracks"
 tracks = track_uris.map { |x| Hallon::Track.new(x) }
-session.wait_for { tracks.all?(&:loaded?) }
 
 # … insert the tracks at the desired position into the playlist.
 playlist.insert(position, tracks)
@@ -41,11 +40,10 @@ playlist.insert(position, tracks)
 # finally, wait for the updates to the playlist to be acknowledged by the
 # Spotify back-end. Once they have, we’ll see the tracks in our desktop
 # client as well.
-#
 puts "Uploading playlist changes to Spotify back-end!"
 
-done = false
-playlist.on(:playlist_update_in_progress) { |x| done = x }
-session.wait_for { done }
+state_changed = false
+playlist.on(:playlist_state_changed) { state_changed = true }
+session.wait_for { state_changed && ! playlist.pending? }
 
 puts "We’re done!"
