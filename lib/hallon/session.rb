Burgestrand/Hallon

commit 3aaac3eecfe8bfdc1faac6a31d3a2077c006fe41

Adjust documentation for Session.initialize and Session.instance

lib/hallon/session.rb
@@ -37,18 +37,21 @@ module Hallon
     # instance at a later time, you can use {instance}.
     #
     # @see Session.instance
-    # @see Session#initialize
     #
     # @param (see Session#initialize)
+    # @option (see Session#initialize)
+    # @yield (see Session#initialize)
+    # @raise (see Session#initialize)
+    # @see (see Session#initialize)
     # @return [Session]
-    def Session.initialize(*args, &block)
+    def Session.initialize(appkey, options = {}, &block)
       raise "Session has already been initialized" if @__instance__
       @__instance__ = new(*args, &block)
     end
 
     # Returns the previously initialized Session.
     #
-    # @see Session.instance
+    # @see Session.initialize
     #
     # @return [Session]
     def Session.instance
@@ -69,15 +72,15 @@ module Hallon
     #
     # @param [#to_s] appkey
     # @param [Hash] options
-    # @option options [String] :user_agent ("Hallon") User-Agent to use (length < 256)
+    # @option options [String] :user_agent ("Hallon") User-Agent to use (length < `256`)
     # @option options [String] :settings_path ("tmp") where to save settings and user-specific cache
-    # @option options [String] :cache_path ("") where to save cache files ("" to disable)
-    # @option options [String] :tracefile (nil) path to libspotify API tracefile (nil to disable)
-    # @option options [String] :device_id (nil) device ID for offline synchronization (nil to disable)
+    # @option options [String] :cache_path ("") where to save cache files (`""` to disable)
+    # @option options [String] :tracefile (nil) path to libspotify API tracefile (`nil` to disable)
+    # @option options [String] :device_id (nil) device ID for offline synchronization (`nil` to disable)
     # @option options [Bool]   :load_playlists (true) load playlists into RAM on startup
     # @option options [Bool]   :compress_playlists (true) compress local copies of playlists
     # @option options [Bool]   :cache_playlist_metadata (true) cache metadata for playlists locally
-    # @yield allows you to define handlers for events (see {Hallon::Base#on})
+    # @yield allows you to define handlers for events (see {Observable#on})
     # @raise [ArgumentError] if `options[:user_agent]` is more than 256 characters long
     # @raise [Hallon::Error] if `sp_session_create` fails
     # @see http://developer.spotify.com/en/libspotify/docs/structsp__session__config.html
@@ -98,7 +101,7 @@ module Hallon
       end
 
       # Set configuration, as well as callbacks
-      config  = Spotify::SessionConfig.new
+      config = Spotify::SessionConfig.new
       config[:api_version]   = Hallon::API_VERSION
       config.application_key = appkey
       @options.each { |(key, value)| config.send(:"#{key}=", value) }

commit 2fd76a6a75801c3e963ea466e9db626cf76d6002

Fix Session.initialize (regression from 3aaac3eecfe8bfdc1faac6a31d3a2077c006fe41)

lib/hallon/session.rb
@@ -46,7 +46,7 @@ module Hallon
     # @return [Session]
     def Session.initialize(appkey, options = {}, &block)
       raise "Session has already been initialized" if @__instance__
-      @__instance__ = new(*args, &block)
+      @__instance__ = new(appkey, options, &block)
     end
 
     # Returns the previously initialized Session.

spec/hallon/session_spec.rb
@@ -16,6 +16,10 @@ describe Hallon::Session do
         Hallon.initialize
       }.to raise_error
     end
+
+    it "should succeed if everything is right" do
+      expect { Hallon::Session.initialize('appkey_good') }.to_not raise_error
+    end
   end
 
   describe ".new" do