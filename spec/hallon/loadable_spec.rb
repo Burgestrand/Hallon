ac6b67ebeb22f418833c243cedf200fa049b36b3
Kim Burgestrand <kim@burgestrand.se>
2012-02-19 23:59:48 +0100

    Add spec for Loadable#load raising Hallon::TimeoutError

--- spec/hallon/loadable_spec.rb
+++ spec/hallon/loadable_spec.rb
@@ -11,11 +11,15 @@ describe Hallon::Loadable do
   end
 
   describe "#load" do
-    it "should timeout if the object does not load in time"
+    it "should timeout if the object does not load in time" do
+      Hallon.stub(:load_timeout).and_return(0.001)
+      loadable.stub(:loaded?).and_return(false)
+      expect { loadable.load }.to raise_error(Hallon::TimeoutError)
+    end
 
     it "should use the Hallon.load_timeout by default" do
-      Timeout.should_receive(:timeout).with(0.01, Hallon::TimeoutError).and_yield
-      Hallon.load_timeout = 0.01
+      Hallon.should_receive(:load_timeout).and_return(0.075)
+      Timeout.should_receive(:timeout).with(0.075, Hallon::TimeoutError).and_yield
       loadable.stub(:loaded?).and_return(true)
       loadable.load
     end
