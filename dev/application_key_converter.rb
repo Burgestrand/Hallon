6649c2fc7bab1c00835981ba5ec5ee8096cfb0c7
Kim Burgestrand <kim@burgestrand.se>
2012-04-24 18:42:38 +0200

    Use pack instead of looping for converting application keys (see #106)

--- dev/application_key_converter.rb
+++ dev/application_key_converter.rb
@@ -1,18 +1,11 @@
 #!/usr/bin/env ruby
 
-require 'pry'
-
 if ARGF.file == STDIN
   puts "You forgot to give me a file!"
   abort "Usage: ruby application_key_converter.rb spotify_appkey.key > new_spotify_appkey.key"
 end
 
-new_format = "".force_encoding('BINARY')
 old_format = ARGF.read.split.join
-
-old_format.each_char.each_slice(2) do |(high, low)|
-  hex = high << low
-  new_format << hex.to_i(16).chr
-end
+new_format = [old_format].pack("H*")
 
 print new_format
